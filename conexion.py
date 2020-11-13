2f63558118493980b4e0701cbaf1a120b3ab5f27
Mario Dueñas <mario.duenas4112@alumnos.udg.mx>
2020-11-12 21:20:05 -0600

    Agregar y mostrar peliculas con y sin id

--- conexion.py
+++ conexion.py
@@ -53,3 +53,56 @@ def iniciar_sesion(correo, contra):
         return id[0], True
     else:
         return None, False
+
+def insertar_peliculas(pelicula):
+    titulo = pelicula['titulo']
+    fecha_visto = pelicula['fecha_visto']
+    imagen = pelicula['imagen']
+    director = pelicula['director']
+    anio = pelicula['anio']
+    idusuario = pelicula['idusuario'] 
+
+    insertar = "INSERT INTO pelicula \
+        (titulo, fecha_visto, imagen, director, anio, idusuario) \
+        VALUES (%s, %s, %s, %s, %s, %s)"
+    cursor.execute(insertar, (titulo, fecha_visto, imagen, director, anio, idusuario))
+    bd.commit()
+
+    if cursor.rowcount:
+        return True
+    else:
+        return False
+
+def get_peliculas():
+    query = "SELECT id_pelicula, titulo, imagen, fecha_visto, director, anio FROM pelicula"
+    cursor.execute(query)
+    peliculas = []
+    for row in cursor.fetchall():
+        pelicula = {
+            'id_pelicula': row[0],
+            'titulo': row[1],
+            'imagen': row[2],
+            'fecha_visto': row[3],
+            'director': row[4],
+            'anio': row[5]   
+        }
+        peliculas.append(pelicula)  
+    return peliculas
+
+def get_pelicula(id):
+    query = "SELECT * FROM pelicula WHERE id_pelicula = %s"
+    cursor.execute(query, (id,))
+    pelicula = {}
+    row = cursor.fetchone()
+    if row:
+        pelicula['id_pelicula'] = row[0]
+        pelicula['titulo'] = row[1]
+        pelicula['fecha_visto'] = row[2]
+        pelicula['imagen'] = row[3]
+        pelicula['director'] = row[4]
+        pelicula['anio'] = row[5]
+        pelicula['valoracion'] = row[6]
+        pelicula['favorito'] = row[7]
+        pelicula['resena'] = row[8]
+        pelicula['compartido'] = row[9]
+    return pelicula    
